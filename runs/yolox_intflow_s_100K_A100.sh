3e2e3d608723b2662920f5f1b4a978184e2714c0
kmjeon <kmjeon@intflow.ai>
2021-08-03 08:00:52 +0900

    prepare training 100K on A100

---
+++ runs/yolox_intflow_s_100K_A100.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+python3 tools/train.py -expn intflow_total_100K -n yolox_intflow_s -f exps/intflow_total_100K/yolox_intflow_s.py -d 8 -b 256 --fp16 -o -c /data/pretrained/yolox_s.pth.tar
