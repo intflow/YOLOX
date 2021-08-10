c3705b50b3a9959d334b3590b74599eb4855a3d3
kmjeon <kmjeon@intflow.ai>
2021-08-10 14:33:55 +0900

    update with amp.init

--- yolox/core/trainer.py
+++ yolox/core/trainer.py
@@ -137,7 +137,8 @@ class Trainer:
         self.optimizer = self.exp.get_optimizer(self.args.batch_size)
 
         if self.amp_training:
-            model, optimizer = amp.initialize(model, self.optimizer, opt_level="O1")
+            amp.init(enabled=True,allow_banned=True)
+            model, optimizer = amp.initialize(model, self.optimizer, opt_level="O1", allow_banned=True)
 
         # value of epoch will be set in `resume_train`
         model = self.resume_train(model)
