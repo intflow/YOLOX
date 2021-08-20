b8ec1c3ada0d169d58ab43ab2a4969e12d468850
kmjeon <kmjeon@intflow.ai>
2021-08-21 04:04:32 +0900

    update runs

--- runs/yolox_intflow_s_100K_2_3090.sh
+++ runs/yolox_intflow_s_100K_2_3090.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-python3 tools/train.py -expn yolox_oad_e2e_s-intflow_total_100K_2 -n yolox_oad_e2e_s -f exps/intflow_oad__total_100K_2/yolox_intflow_s.py -d 4 -b 32 \
+python3 tools/train.py -expn yolox_oad_e2e_s-intflow_total_100K_2 -n yolox_oad_e2e_s -f exps/intflow_oad__total_100K_2/yolox_intflow_s.py -d 4 -b 128 --fp16 -o \
 -c /data/pretrained/hcow/yolox_intflow_s-intflow_total_100K.pth.tar
--- runs/yolox_intflow_s_100K_A100.sh
+++ runs/yolox_intflow_s_100K_A100.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-python3 tools/train.py -expn yolox_oad_e2e_s-intflow_total_100K -n yolox_oad_e2e_s -f exps/intflow_oad__total_100K/yolox_intflow_s.py -d 8 -b 256 \
+python3 tools/train.py -expn yolox_oad_e2e_s-intflow_total_100K -n yolox_oad_e2e_s -f exps/intflow_oad__total_100K/yolox_intflow_s.py -d 8 -b 384 --fp16 -o \
 -c /data/pretrained/hcow/yolox_intflow_s-intflow_total_100K.pth.tar
