6fa4ade355a5c8d41c4067374b9ddf36a9d36b0d
kmjeon <kmjeon@intflow.ai>
2021-08-22 23:11:20 +0900

    turned off fp16

--- runs/yolox_intflow_s_100K_2_3090.sh
+++ runs/yolox_intflow_s_100K_2_3090.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-python3 tools/train.py -expn yolox_oad_e2e_s-intflow_total_100K_2 -n yolox_oad_e2e_s -f exps/intflow_oad__total_100K_2/yolox_intflow_s.py -d 4 -b 32 --fp16 --cache \
+python3 tools/train.py -expn yolox_oad_e2e_s-intflow_total_100K_2 -n yolox_oad_e2e_s -f exps/intflow_oad__total_100K_2/yolox_intflow_s.py -d 4 -b 32 --cache \
 -c /data/pretrained/yolox_s.pth
--- runs/yolox_intflow_s_100K_A100.sh
+++ runs/yolox_intflow_s_100K_A100.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-python3 tools/train.py -expn yolox_oad_e2e_s-intflow_total_100K -n yolox_oad_e2e_s -f exps/intflow_oad__total_100K/yolox_intflow_s.py -d 8 -b 64 --fp16 --cache \
+python3 tools/train.py -expn yolox_oad_e2e_s-intflow_total_100K -n yolox_oad_e2e_s -f exps/intflow_oad__total_100K/yolox_intflow_s.py -d 8 -b 64 --cache \
 -c /data/pretrained/yolox_s.pth
