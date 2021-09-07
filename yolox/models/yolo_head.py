1242b800cd43896bb935e0d5606d500cd1b80c7f
kmjeon <kmjeon@intflow.ai>
2021-09-08 00:59:08 +0900

    retry with img

--- yolox/models/yolo_head.py
+++ yolox/models/yolo_head.py
@@ -496,7 +496,7 @@ class YOLOXHead(nn.Module):
             loss_l1 = 0.0
 
         reg_weight = 5.0
-        RAD_LM = False
+        RAD_LM = True
         if RAD_LM == True:
             rad_weight = 1.0 
             lm_weight = 0.15
