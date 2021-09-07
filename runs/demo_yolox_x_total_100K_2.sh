d6a2f43c837de4c3297e81c219f0b194f0eeb875
kmjeon <kmjeon@intflow.ai>
2021-09-08 01:51:25 +0900

    evaluatin eval with threshold

--- runs/demo_yolox_x_total_100K_2.sh
+++ runs/demo_yolox_x_total_100K_2.sh
@@ -33,5 +33,5 @@ python3 tools/demo.py --demo video -expn yolox_x_oad_lm3__intflow_total_100K_2 -
 -c /data/pretrained/hcow/yolox_x_oad_lm3__intflow_total_100K_2_test2.pth \
 --path '/data/test_videos/[gwangjufarm_breeding 1번] 2021. 9. 7. 오후 1_40_34.mp4' \
 --save_folder /data/yolox_out \
---conf 0.6 --nms 0.45 --tsize 640 --save_result --device gpu
+--conf 0.3 --nms 0.3 --tsize 640 --save_result --device gpu
 
---
+++ runs/eval_yolox_intflow_x_100K_2.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+python3 tools/eval.py -expn yolox_x_oad_lm3__intflow_total_100K_2 -n yolox_x_oad_lm3 \
+-f exps/yolox_oad_lm3__intflow_total_100K_2/yolox_x_oad_lm3.py -d 4 -b 32 --fp16 \
+-c /data/pretrained/hcow/yolox_x_oad_lm3__intflow_total_100K_2_test2.pth \
+--conf 0.01 --nms 0.65
