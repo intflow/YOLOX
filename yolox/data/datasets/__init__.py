fe5e484f9d3707482c914d5df88f4246ad035ff1
kmjeon <kmjeon@intflow.ai>
2021-09-11 23:18:44 +0900

    bug fix

--- yolox/data/datasets/__init__.py
+++ yolox/data/datasets/__init__.py
@@ -6,7 +6,6 @@ from .coco import COCODataset
 from .coco_classes import COCO_CLASSES
 from .intflow import INTFLOWDataset
 from .intflow_classes import INTFLOW_CLASSES
-from .crowdhuman_classes import CROWDHUMAN_CLASSES
 from .datasets_wrapper import ConcatDataset, Dataset, MixConcatDataset
 from .mosaicdetection import MosaicDetection
 from .voc import VOCDetection
